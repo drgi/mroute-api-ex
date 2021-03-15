8dfca4d1e1fabfa70f794202a76f3d25c83fe39a
Dr <andrey_pushkin@mail.ru>
2021-03-15 12:23:46 +0300

    fs module

---
+++ controllers/fileSystem.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const DEFAULT_DIR = path.resolve(__dirname, '../public');
+const ROUTES_ROOT_DIR = path.resolve(__dirname, '../public/routes/');
+const ROUTES_DIRS = ['routefiles', 'routeimages', 'pointimages'];
+
+const mkRouteDirectories = (routeId) => {};
+
+//iside methods
+
+const _mkDirectory = (path, dirName) => {};
